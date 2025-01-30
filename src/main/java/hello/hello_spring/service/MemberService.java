a996238f19f2a21d9b0d0182aef99bf0c840b250
YoungdanNoh <shdbwjd705270@gmail.com>
2025-01-30 23:37:34 +0900

    회원 관리 예제 - 백엔드 개발

---
+++ src/main/java/hello/hello_spring/service/MemberService.java
@@ -0,0 +1,47 @@
+package hello.hello_spring.service;
+
+import hello.hello_spring.domain.Member;
+import hello.hello_spring.repository.MemberRepository;
+import hello.hello_spring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    //@Autowired
+    public MemberService(MemberRepository memberRepository){ //생성자 사용, 이러한 것을 Dependency Injection이라고 한다.
+        this.memberRepository = memberRepository;
+    }
+
+    /*
+    *  회원 가입
+    * */
+    public Long join(Member member) {
+        //같은 이름이 있는 중복 회원X
+        validateDuplicateMember(member);
+
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw  new IllegalStateException("Member already exists");
+                });
+    }
+
+    /*
+    * 전체 회원 조회
+    * */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
